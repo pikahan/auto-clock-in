dd32b5aaa4a00f8452605fd5ee29e22c8cc2e1e8
lingyi <yhl990104@qq.com>
2020-12-10 17:05:26 +0800

    feat: 自动设置定时任务

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const childProcess = require('child_process')
 const config = require('./config')
+const schedule = require("node-schedule");
 
-for (let i = 0; i < config.userInfo.length; i++) {
-  const userData = config.userInfo[i]
-  childProcess.fork(__dirname + '/server.js', [userData.username, userData.password])
+const rule = new schedule.RecurrenceRule();
+
+const task = () => {
+  for (let i = 0; i < config.userInfo.length; i++) {
+    const userData = config.userInfo[i]
+    childProcess.fork(__dirname + '/server.js', [userData.username, userData.password])
+  }
 }
+
+rule.dayOfWeek = [new schedule.Range(0, 6)];
+rule.hour = 10;
+rule.minute = 0;
+schedule.scheduleJob(rule, () => {
+  task();
+})
